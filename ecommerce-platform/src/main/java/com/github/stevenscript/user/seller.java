8811f118291939f555a5916712c72a12fbeb84be
Cameron Beanland <cameronbeanland@hotmail.com>
2024-12-09 16:31:17 -0330

    Update seller.java
    -A role-based class revolving around the 'seller', when a user logs in and this is their role, directs them to the dashboard where they can sell/manage inventory and check sales

--- ecommerce-platform/src/main/java/com/github/stevenscript/user/seller.java
+++ ecommerce-platform/src/main/java/com/github/stevenscript/user/seller.java
@@ -1,5 +1,7 @@
 package com.github.stevenscript.user;
 
-public class seller {
-    
+public class seller extends user {
+    public seller() {               // tiny object used to verify whether someone is of 'seller' status,
+        this.setRole("seller");     // and will be redirected to the sellers menu.
+    }
 }
