e3f7cd8599b30cdc0995e3d938da4530c33b5388
Cameron Beanland <cameronbeanland@hotmail.com>
2024-12-09 16:27:04 -0330

    Update admin.java
    -Added the admin class, very small line of code that is used for userService. This is just used to verify whether a potential user is a 'buyer', 'seller', or 'admin' and directs them to the required dashboard

--- ecommerce-platform/src/main/java/com/github/stevenscript/user/admin.java
+++ ecommerce-platform/src/main/java/com/github/stevenscript/user/admin.java
@@ -1,5 +1,7 @@
 package com.github.stevenscript.user;
 
-public class admin {
-    
+public class admin extends user {
+    public admin() {                // a little object class that verifies whether an 'admin' is logged in
+        this.setRole("admin");      // which will then redirect them into the admin menu.
+    }
 }
