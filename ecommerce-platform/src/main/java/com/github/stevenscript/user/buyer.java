932adeaf5a55154197f5b0a5e665563de622ca05
Cameron Beanland <cameronbeanland@hotmail.com>
2024-12-09 16:29:41 -0330

    Update buyer.java
    -A small role-based object class, simply to verify that when a user logs in, they'll be directed to the 'buyer' dashboard

--- ecommerce-platform/src/main/java/com/github/stevenscript/user/buyer.java
+++ ecommerce-platform/src/main/java/com/github/stevenscript/user/buyer.java
@@ -1,5 +1,7 @@
 package com.github.stevenscript.user;
 
-public class buyer {
-    
+public class buyer extends user {
+    public buyer() {                // small object class indicating that a 'buyer' user will be
+        this.setRole("buyer");      // logged in and redirected towards the buyer menu (found in userService class).
+    }
 }
