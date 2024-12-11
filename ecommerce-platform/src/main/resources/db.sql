e7ed1415db13009d5aa80a68fca774c2e7984bb9
EmilyWProjects <Emily.Warford@keyin.com>
2024-12-11 17:27:50 -0330

    Added SQL queries

---
+++ ecommerce-platform/src/main/resources/db.sql
@@ -0,0 +1,37 @@
+
+--Tables--
+
+--Users
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255),
+    password VARCHAR(255),
+    email VARCHAR(255),
+    role VARCHAR(255)
+);
+
+--Products
+CREATE TABLE IF NOT EXISTS products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    price NUMBER,
+    quantity INT,
+    seller_id INT
+);
+
+
+--Queries--
+
+--Users
+INSERT INTO users (username, password, email, role) VALUES (?, ?, ?, ?)
+SELECT * FROM users WHERE username = ?
+DELETE FROM users WHERE username = ?
+
+--Products
+SELECT id, name, price, quantity, seller_id FROM products
+INSERT INTO products (name, price, quantity, seller_id) VALUES (?, ?, ?, ?) RETURNING id
+SELECT id, name, price, quantity, seller_id FROM products WHERE id = ?
+SELECT id, name, price, quantity, seller_id FROM products WHERE seller_id = ?
+SELECT id, name, price, quantity, seller_id FROM products WHERE name ILIKE ?
+UPDATE products SET name = ?, price = ?, quantity = ? WHERE id = ?
+DELETE FROM products WHERE id = ?
